07c1099ed112d5b6cd78ab746a6f396236ef1a09
Guilherme Tassinari <guilhermehen.tassinari@gmail.com>
2025-08-09 23:00:18 -0300

    inserted validation for some fields and error handling for the API

--- src/main/java/br/com/guilhermetassinari/gestao_vagas/modules/candidate/CandidateEntity.java
+++ src/main/java/br/com/guilhermetassinari/gestao_vagas/modules/candidate/CandidateEntity.java
@@ -1,15 +1,24 @@
 package br.com.guilhermetassinari.gestao_vagas.modules.candidate;
 
 import java.util.UUID;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.Pattern;
 import lombok.Data;
+import org.hibernate.validator.constraints.Length;
 
 @Data
 public class CandidateEntity {
 
     private UUID id;
     private String name;
+
+    @Pattern(regexp = "\\S+", message = "The field [username] must not contain spaces")
     private String username;
+    @Email(message = "The field [email] must be a valid e-mail")
     private String email;
+
+    @Length(min = 10, max = 100)
     private String password;
     private String description;
     private String curriculum;
