b097e2804631c10ef71b96f474b910a3021a28d6
Guilherme Tassinari <guilhermehen.tassinari@gmail.com>
2025-08-26 23:38:04 -0300

    Implemented JWT inside the useCase, and created an /auth/company route

--- src/main/java/br/com/guilhermetassinari/gestao_vagas/modules/company/useCases/AuthCompanyUseCase.java
+++ src/main/java/br/com/guilhermetassinari/gestao_vagas/modules/company/useCases/AuthCompanyUseCase.java
@@ -2,29 +2,64 @@ package br.com.guilhermetassinari.gestao_vagas.modules.company.useCases;
 
 import br.com.guilhermetassinari.gestao_vagas.modules.company.dto.AuthCompanyDTO;
 import br.com.guilhermetassinari.gestao_vagas.modules.company.repositories.CompanyRepository;
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.authentication.BadCredentialsException;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+
+import java.time.Duration;
+import java.time.Instant;
+import java.util.function.Supplier;
 
 @Service
 @RequiredArgsConstructor
 public class AuthCompanyUseCase {
 
+    @Value("${security.token.secret}")
+    private String secretKey;
+
+    @Value("${security.token.expiration:3600}")
+    private Long tokenExpirationInSeconds;
+
     private final CompanyRepository companyRepository;
 
     private final PasswordEncoder passwordEncoder;
 
-    public void execute(AuthCompanyDTO authCompanyDTO) {
-        var company = this.companyRepository.findByUsername(authCompanyDTO.getUsername()).orElseThrow(
-                () -> new UsernameNotFoundException("Company not found")
-        );
+    public String execute(AuthCompanyDTO authCompanyDTO) {
+        var company = this.companyRepository.findByUsername(authCompanyDTO.getUsername());
 
-        var passwordMatches = this.passwordEncoder.matches(authCompanyDTO.getPassword(), company.getPassword());
+        // Supplier postpone the execution, being a function that doesn't receive any arguments. But that returns a value
+        // It's function it's to postpone de operation execution
+        Supplier<BadCredentialsException> exception =
+                () -> new BadCredentialsException("Username/Password is incorrect");
 
-        if(!passwordMatches) {
-            throw new BadCredentialsException("Invalid credentials");
+        if (company.isEmpty()) {
+            throw exception.get();
         }
+
+        var foundCompany = company.get();
+
+        if (!this.passwordEncoder.matches(authCompanyDTO.getPassword(), foundCompany.getPassword())) {
+            throw exception.get();
+        }
+
+        return generateToken(foundCompany.getId().toString());
+    }
+
+    private String generateToken(String companyId){
+        Algorithm algorithm = Algorithm.HMAC256(secretKey);
+        Instant now = Instant.now();
+        Instant expiration = now.plus(Duration.ofSeconds(tokenExpirationInSeconds));
+
+        return JWT.create()
+                .withIssuer("javagas")
+                .withSubject(companyId)
+                .withIssuedAt(now)
+                .withExpiresAt(expiration)
+                .sign(algorithm);
+
     }
 }
