99c7f78344e1e1ac432674b2697682427a701af4
Guilherme Tassinari <guilhermehen.tassinari@gmail.com>
2025-09-07 01:11:47 -0300

    Inserted examples inside the schema, so the Swagger shows an example of what input it expects when using the API route

--- src/main/java/br/com/guilhermetassinari/gestao_vagas/modules/company/controllers/JobController.java
+++ src/main/java/br/com/guilhermetassinari/gestao_vagas/modules/company/controllers/JobController.java
@@ -3,6 +3,14 @@ package br.com.guilhermetassinari.gestao_vagas.modules.company.controllers;
 import br.com.guilhermetassinari.gestao_vagas.modules.company.dto.CreateJobDTO;
 import br.com.guilhermetassinari.gestao_vagas.modules.company.entities.JobEntity;
 import br.com.guilhermetassinari.gestao_vagas.modules.company.useCases.CreateJobUseCase;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.ArraySchema;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
@@ -15,14 +23,23 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.UUID;
 
 @RestController
-@RequestMapping("/company")
+@RequestMapping("/company/job")
 @RequiredArgsConstructor
 public class JobController {
 
     private final CreateJobUseCase createJobUseCase;
 
-    @PostMapping("/job")
+    @PostMapping("/")
     @PreAuthorize("hasRole('COMPANY')")
+    @Tag(name = "Jobs", description = "Jobs information")
+    @Operation(summary =  "Job registration",
+            description = "This function is responsible to register a job vacancy inside the company")
+    @ApiResponses(@ApiResponse(responseCode = "200", content = {
+            @Content(
+                    array = @ArraySchema(schema = @Schema(implementation = JobEntity.class))
+            )
+    }))
+    @SecurityRequirement(name = "jwt_auth")
     public JobEntity create(@Valid @RequestBody CreateJobDTO createJobDTO, HttpServletRequest request){
         var companyId = request.getAttribute("company_id");
 
--- src/main/java/br/com/guilhermetassinari/gestao_vagas/modules/company/dto/CreateJobDTO.java
+++ src/main/java/br/com/guilhermetassinari/gestao_vagas/modules/company/dto/CreateJobDTO.java
@@ -1,10 +1,15 @@
 package br.com.guilhermetassinari.gestao_vagas.modules.company.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Data;
 
 @Data
 public class CreateJobDTO {
+
+    @Schema(example = "Job vacancy for a junior developer", requiredMode = Schema.RequiredMode.REQUIRED)
     private String description;
+    @Schema(example = "GYMPASS, Health Plan", requiredMode = Schema.RequiredMode.REQUIRED)
     private String benefits;
+    @Schema(example = "JUNIOR", requiredMode = Schema.RequiredMode.REQUIRED)
     private String level;
 }
