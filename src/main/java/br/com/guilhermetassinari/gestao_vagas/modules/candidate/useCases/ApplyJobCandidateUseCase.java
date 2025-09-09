b80b6bf819421be1330c1b2682fef4487b60eaa2
Guilherme Tassinari <guilhermehen.tassinari@gmail.com>
2025-09-09 10:54:13 -0300

    Created the ApplyJobCandidateUseCase, so the method execute for a candidate applying to a job is created

---
+++ src/main/java/br/com/guilhermetassinari/gestao_vagas/modules/candidate/useCases/ApplyJobCandidateUseCase.java
@@ -0,0 +1,25 @@
+package br.com.guilhermetassinari.gestao_vagas.modules.candidate.useCases;
+
+import br.com.guilhermetassinari.gestao_vagas.exceptions.JobNotFoundException;
+import br.com.guilhermetassinari.gestao_vagas.exceptions.UserNotFoundException;
+import br.com.guilhermetassinari.gestao_vagas.modules.candidate.repositories.CandidateRepository;
+import br.com.guilhermetassinari.gestao_vagas.modules.company.repositories.JobRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.UUID;
+
+@Service
+@RequiredArgsConstructor
+public class ApplyJobCandidateUseCase {
+
+    private final CandidateRepository candidateRepository;
+
+    private final JobRepository jobRepository;
+
+    public void execute(UUID idCandidate, UUID idJob){
+        this.candidateRepository.findById(idCandidate).orElseThrow(UserNotFoundException::new);
+
+        this.jobRepository.findById(idJob).orElseThrow(JobNotFoundException::new);
+    }
+}
